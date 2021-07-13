7f1589181a60e893ce86cab09cc4bb2401fb1fab
Zaka20113382 <zaka13zulkarnaen@gmail.com>
2021-07-13 15:44:10 +0700

    Add project files.

---
+++ ProjectMahasiswa/Mahasiswa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectMahasiswa
+{
+    public class Mahasiswa
+    {
+       // PERINTAH: lengkapi property class mahasiswa, sesuai petunjuk soal
+            public string nama { get; set; }
+            public float nim { get; set; }
+            public string jenis { get; set; }
+            public float ipk { get; set; }
+        }
+}
